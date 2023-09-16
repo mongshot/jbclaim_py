9e50454d406f4b63fdcbb7308a32757b0c61a9c8
mongshot <62374821+mongshot@users.noreply.github.com>
2023-09-16 14:27:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
-from flask import Flask, render_template
+TELEGRAM_BOT_TOKEN = '6560335312:AAHo82hdFJr1q_6CKUkms7NkL68kwgMul08'
+TELEGRAM_CHAT_ID = '71046013'
+
+from flask import Flask, render_template, request
+from telegram import Bot
 
 app = Flask(__name__)
+app.debug = True
 
-@app.route("/")
-def index():
-    return render_template('./index.html')
+TELEGRAM_BOT_TOKEN = '6560335312:AAHo82hdFJr1q_6CKUkms7NkL68kwgMul08'
+TELEGRAM_CHAT_ID = '71046013'
+
+bot = Bot(token=TELEGRAM_BOT_TOKEN)
+
+@app.route('/')
+def home():
+    return render_template('report.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    reporter = request.form['reporter']
+    location = request.form['location']
+    complaint = request.form['complaint']
+
+    message = f'신고자: {reporter}\n위치: {location}\n불편내용: {complaint}'
+
+    send_telegram_message(message)
+
+    return '신고가 접수되었습니다. 감사합니다!'
+
+def send_telegram_message(message):
+    bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
+
+if __name__ == '__main__':
+    app.run()
