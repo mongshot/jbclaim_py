4f0f76423b22eb90ece8d6c021cc8abcd76cafe4
mongshot <62374821+mongshot@users.noreply.github.com>
2023-09-16 16:30:51 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from flask import Flask, render_template, request
 from telegram import Bot
-import asyncio  # 비동기 작업을 위해 asyncio 모듈을 가져옵니다.
+import asyncio
+import os  # 환경 변수 사용을 위해 os 모듈을 가져옵니다.
 
 app = Flask(__name__)
 app.debug = True
 
+# 환경 변수에서 토큰과 채팅 ID를 가져옵니다.
 TELEGRAM_BOT_TOKEN = '6560335312:AAHo82hdFJr1q_6CKUkms7NkL68kwgMul08'
 TELEGRAM_CHAT_ID = '71046013'
+
+# 환경 변수가 설정되어 있지 않으면 기본 값을 사용합니다.
+if not TELEGRAM_BOT_TOKEN:
+    TELEGRAM_BOT_TOKEN = 'YOUR_DEFAULT_BOT_TOKEN'
+
+if not TELEGRAM_CHAT_ID:
+    TELEGRAM_CHAT_ID = 'YOUR_DEFAULT_CHAT_ID'
+
 bot = Bot(token=TELEGRAM_BOT_TOKEN)
 
 @app.route('/')
@@ -21,13 +31,15 @@ def submit():
 
     message = f'신고인: {reporter}\n신고위치: {location}\n신고사항: {complaint}'
 
-    loop = asyncio.new_event_loop()  # 비동기 작업을 위한 이벤트 루프를 생성합니다.
-    asyncio.set_event_loop(loop)  # 현재 이벤트 루프를 설정합니다.
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
     try:
-        loop.run_until_complete(send_telegram_message(message))  # 메시지 전송을 비동기로 실행합니다.
-    finally:
-        loop.close()  # 이벤트 루프를 닫습니다.
+        loop.run_until_complete(send_telegram_message(message))
+    except Exception as e:
+        # 예외 처리를 통해 오류 메시지를 기록하고 사용자에게 오류를 알릴 수 있습니다.
+        error_message = f'오류 발생: {str(e)}'
+        return error_message
 
     return '신고가 접수되었습니다. 감사합니다!'
 
